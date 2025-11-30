d74a3d3760227ed87a6784017f1d86bd37c82aff
Ganesh Kotalwar <25142682@studentmail.ul.ie>
2025-11-30 02:25:20 +0000

    Minor change

--- user-service/src/main/java/com/rideshare/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/rideshare/userservice/UserServiceApplication.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EntityScan(basePackages = "com.rideshare.userservice.model")
 @EnableJpaRepositories(basePackages = "com.rideshare.userservice.repository")
+
+//Main Method
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
